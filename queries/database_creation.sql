0fbd299e4526138380314266cea335c8dd7303af
Ferdinand Leube <leubeb@gmail.com>
2022-05-21 14:05:16 +0200

    finished code and organization of queries

---
+++ queries/database_creation.sql
@@ -0,0 +1,45 @@
+# in this query we set up our databse, created the first main tables consisting of our primary data
+# we tried to encode the upload of our table data into the table data, but do to technical issues we had to input the data manually
+
+
+create database if not exists database_Science_and_Tech_CI_test2;
+use database_Science_and_Tech_CI_test2; 
+create table Indicator_1 (
+country_ID varchar(20),
+YR2014 float,
+YR2015 float,
+YR2016 float,
+YR2017 float,
+YR2018 float);
+select * from indicator_2;
+create table Indicator_2 (
+country_ID varchar(20),
+YR2014 float,
+YR2015 float,
+YR2016 float,
+YR2017 float,
+YR2018 float);
+select * from indicator_2;
+create table Indicator_3 (
+country_ID varchar(20),
+YR2014 float,
+YR2015 float,
+YR2016 float,
+YR2017 float,
+YR2018 float);
+select * from indicator_3;
+create table Indicator_4 (
+country_ID varchar(20),
+YR2014 float,
+YR2015 float,
+YR2016 float,
+YR2017 float,
+YR2018 float);
+select * from indicator_4;
+create table Indicator_5 (
+country_ID varchar(20),
+YR2014 float,
+YR2015 float,
+YR2016 float,
+YR2017 float,
+YR2018 float);
